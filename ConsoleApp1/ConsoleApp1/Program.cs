3eed9327c4fed709860cd45271597486b79cd56c
Pavel Limonya <pavel.limonya@gmail.com>
2019-02-13 22:56:14 +0300

    dfdfdfdfdf

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("fghdhhdgfhgdhf");
             Console.WriteLine(@"Hello   
 my dear friend.
 ..");
